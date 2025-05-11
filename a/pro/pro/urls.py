bc4bc43a020e66847ae9353766984b918b233d7c
FabianMolinaa <133251030+FabianMolinaa@users.noreply.github.com>
2025-05-11 12:00:55 -0400

    Update urls.py

--- a/pro/pro/urls.py
+++ a/pro/pro/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('appp/',include("appp.urls")),
+    path('appp/',include("appp.urls")), """ urls de la app"""
     path("", include("appp.urls")),
     path("accounts/", include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
