397bcaaaff14c5a0a9fa00d6333212c0eff83c71
FabianMolinaa <133251030+FabianMolinaa@users.noreply.github.com>
2025-05-11 12:28:09 -0400

    Update admin.py

--- a/pro/appp/admin.py
+++ a/pro/appp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Producto,CarritoItem,Compra, DetalleCompra
 
-
+#base de datos
 admin.site.register(Producto)
 admin.site.register(CarritoItem)
 admin.site.register(Compra)
